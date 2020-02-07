9db405443e81736677f7c0b8800aa1babd69c38e
daseul bae <ektmf1993@gmail.com>
2020-02-07 16:07:57 +0900

    develop

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created By ds on 2020/02/07.
+ */
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/home")
+    public ResponseEntity<String> home() {
+        return ResponseEntity.ok("hello");
+    }
+}
